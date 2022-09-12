2eb836f027e8f9264d5c9c87e2fbaafeecfd5b93
Ayoe <dannyhopkins20003@gmail.com>
2022-09-12 14:55:45 +0100

    added templates

--- rezumify/src/components/Home.tsx
+++ rezumify/src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Rings } from 'react-loader-spinner'
+import document from '../Assets/document.png'
 
 const Home = () => {
     const [loading, setLoading] = useState<boolean>(false)
@@ -11,14 +12,18 @@ const Home = () => {
         setTimeout(() => {
             navigate('/templates')
             setLoading(false)
-        }, 3000);
+        }, 4000);
     }
 
-    if (loading) return <Rings height="80" width="80"  color="#4fa94d" radius="6" wrapperStyle={{}} wrapperClass="" visible={true} ariaLabel="rings-loading" />   
+    if (loading) return <Rings height="100" width="100"  color="#0006cc" radius="6" wrapperStyle={{}} wrapperClass="" visible={true} ariaLabel="rings-loading" />   
 
   return (
     <div>
         <h1>Build your Resume In 3 Easy Steps</h1>
+
+        <div>
+            <img src={document} alt='document'/>
+        </div>
 
         <ul>
             <li>Select a template from our collection</li>
